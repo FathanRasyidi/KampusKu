e6b3c70ed91f66ce4b14e6214c06de5e3908027e
FathanRasyidi <124230089@student.upnyk.ac.id>
2025-09-09 11:11:54 +0700

    Update minSdkVersion to use flutter.minSdkVersion and upgrade dependencies in pubspec.lock

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.bnsp"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 34
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
